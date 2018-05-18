5e15c45970eb1fa074007051462cd099428d7b3d
Katie Maxwell <katie@codeinstitute.net>
2018-05-18 15:13:02 +0000

    movie detail page added

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-// $( document ).ready(function() {
-//     $(".results").hover(function() {
-//     $(this).addClass("display_none")
-// });
-// });
-
+function doRequestMain(funcToCall, genreArg="", yearStartArg=1, yearEndArg=2099, pageNo=1) {
+    console.log(pageNo)
+    let request = new XMLHttpRequest();
+    let rootURL = "https://api.themoviedb.org/3/discover/movie?api_key=e5dce9ac19487be2b65ceb7be99e8ca7"
+    request.open("GET", rootURL + "&with_genres=" + genreArg + "&primary_release_date.gte=" + yearStartArg + "&primary_release_date.lte=" + yearEndArg+ "&page=" + pageNo)
+    request.send();
+    request.onreadystatechange= function (){
+        if (this.readyState == 4 && this.status == 200){
+            funcToCall(this.responseText, genreArg, yearStartArg, yearEndArg, pageNo)
+        }
+    }
+    // console.log(request)
+}
 
 function doRequestGenres(funcToCall) {
     let request = new XMLHttpRequest();
@@ -16,17 +23,18 @@ function doRequestGenres(funcToCall) {
     }
 }
 
-function doRequestMain(funcToCall, genreArg="", yearStartArg=1, yearEndArg=2099) {
+function doRequestSpecificMovie(funcToCall, movieId) {
     let request = new XMLHttpRequest();
-    let rootURL = "https://api.themoviedb.org/3/discover/movie?api_key=e5dce9ac19487be2b65ceb7be99e8ca7"
-    request.open("GET", rootURL + "&with_genres=" + genreArg + "&primary_release_date.gte=" + yearStartArg + "&primary_release_date.lte=" + yearEndArg)
+    request.open("GET", "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=e5dce9ac19487be2b65ceb7be99e8ca7&language=en-US" )
     request.send();
     request.onreadystatechange= function (){
         if (this.readyState == 4 && this.status == 200){
-            funcToCall(this.responseText, genreArg, yearStartArg, yearEndArg)
+            funcToCall(this.responseText)
         }
     }
+    console.log(request)
 }
+
 
 function mainFunc(apiData) {
     let showData = JSON.parse(apiData);
@@ -81,28 +89,32 @@ function yearSelect(data, genreIdsString) {
     let yearCanvas = document.getElementById('year_canvas')
 
     yearCanvas.innerHTML = "<h3>Choose A Release Year</h3>"
-    yearCanvas.innerHTML += "<button class='btn btn-warning' onclick='yearAdapt([" + genreIdsString + "],1,1979)'> 70s & older </button>"
-    yearCanvas.innerHTML += "<button class='btn btn-warning' onclick='yearAdapt([" + genreIdsString + "],1980,1989)'> 80s </button>"
-    yearCanvas.innerHTML += "<button class='btn btn-warning' onclick='yearAdapt([" + genreIdsString + "],1990,1999)'> 90s </button>"
-    yearCanvas.innerHTML += "<button class='btn btn-warning' onclick='yearAdapt([" + genreIdsString + "],2000,2009)'> 00s </button>"
-    yearCanvas.innerHTML += "<button class='btn btn-warning' onclick='yearAdapt([" + genreIdsString + "],2010,2019)'> 10s </button>"
-    yearCanvas.innerHTML += "<button class='btn btn-warning' onclick='yearAdapt([" + genreIdsString + "]," + '"' +currentYear+'-'+currentMonth +'-'+currentDate + '"' + ",2099)'> Unreleased </button>"
+    yearCanvas.innerHTML += "<button class='btn btn-warning year_button' onclick='yearAdapt([" + genreIdsString + "],1,2099); makeActive()'> All </button>"
+    yearCanvas.innerHTML += "<button class='btn btn-warning year_button' onclick='yearAdapt([" + genreIdsString + "],1,1979); makeActive()'> 70s & older </button>"
+    yearCanvas.innerHTML += "<button class='btn btn-warning year_button' onclick='yearAdapt([" + genreIdsString + "],1980,1989); makeActive()'> 80s </button>"
+    yearCanvas.innerHTML += "<button class='btn btn-warning year_button' onclick='yearAdapt([" + genreIdsString + "],1990,1999); makeActive()'> 90s </button>"
+    yearCanvas.innerHTML += "<button class='btn btn-warning year_button' onclick='yearAdapt([" + genreIdsString + "],2000,2009); makeActive()'> 00s </button>"
+    yearCanvas.innerHTML += "<button class='btn btn-warning year_button' onclick='yearAdapt([" + genreIdsString + "],2010,2019); makeActive()'> 10s </button>"
+    yearCanvas.innerHTML += "<button class='btn btn-warning year_button' onclick='yearAdapt([" + genreIdsString + "]," + '"' +currentYear+'-'+currentMonth +'-'+currentDate + '"' + ",2099); makeActive()'> Unreleased </button>"
 }
-
+function makeActive() {
+    $(this).css("background-color", "blue")
+}
 function yearAdapt(genreIdsArray, yearLow, yearHigh) {
     let genres = genreIdsArray.join()
     doRequestMain(showMovies, genres, yearLow, yearHigh)
-    
 }
 
-function showMovies(data) {
+function showMovies(data, genre, yearStart, yearEnd, pageNo) {
+    console.log(genre, yearStart, yearEnd, pageNo)
     let showData = JSON.parse(data)
-    console.log(showData)
+    
     updateNumberOfMovies(showData.total_results)
     let resultsCanvas = document.getElementById('results_canvas')
-    let results ="<h3>Top 20 Results</h3>"
+    let results =""
     for (let i = 0; i <= showData.results.length -1; i++) {
-        results += "<div class='results col-sm-3'>"
+        let movieId = showData.results[i].id
+        results += "<a onclick='doRequestSpecificMovie(movieDetail, " + movieId + ")'><div class='results col-sm-3'>"
         if (showData.results[i].poster_path == null) {
         results += "<img class='image placeholder_img' src='placeholder.jpg' width='200'>"
         }else {
@@ -110,127 +122,52 @@ function showMovies(data) {
         }
         results += "<div id='results_info'><div class='text'>"
         results += "<h3>" + showData.results[i].title + "<h3><h5>Release Date:<br>" + showData.results[i].release_date + "</h5>" + "<h5>Vote Average:<br>" + showData.results[i].vote_average + "</h5>" 
-        results += "</div></div></div>"
-        
-        
-        // results += "<h5 class='title_in_placeholder_img'>" + showData.results[i].release_date + "</h5></div>" 
-        // results += "<div class='info'>" + showData.results[i].title + "</div>"
-        // results += "<h5>" + showData.results[i].release_date + "</h5></div>"
+        results += "</div></div></div></a>"
      }
     document.getElementById('results_canvas').innerHTML = results
+    // console.log(showData.total_pages)
+    let paginationArray = []
+    for(let i = 1; i <= showData.total_pages; i++) {
+        paginationArray.push(i)
+    }
+    let pageGroup = paginationArray.slice(0,10)
+    // console.log(pageGroup)
+    let pagination = "<button onclick='' disabled>&laquo;</button>";
     
+    for(let i = 1; i <= pageGroup.length; i++) {
+        pagination += "<button id='pageNo" + i + "' onclick='changePage("+ i + ",[" + genre + "]," + yearStart + "," + yearEnd +")' >" + i + "</button>"
+    }
+    
+    pagination += "<button onclick=''>&raquo;</button>";
+     
+    $(".pagination_center").removeClass("display_none")
+    document.getElementById("pagination").innerHTML = pagination;
+}
+
+function changePage(currentPage, genre, yearStart, yearEnd){
+    let currentPageId = "#pageNo" + currentPage
+    $(currentPageId).css("background-color", "blue")
+    doRequestMain(showMovies, genre, yearStart, yearEnd, currentPage)
 }
 
 
+function movieDetail(apiData) {
+    $('.search').hide()
+    $('.movie_detail').show()
+    let data = JSON.parse(apiData)
+    console.log(data)
+    let movie = "<a href='#' onclick='goBack()'>Go Back to Search</a>"
+    movie += "<h1>"+ data.title +"</h1>"
+    movie += "<img src='https://image.tmdb.org/t/p/w300_and_h450_bestv2" + data.poster_path + "' width='200'>"
+    movie += "<p>"+ data.overview +"</p>"
+    
+    document.getElementById("movie_canvas").innerHTML = movie
+}
 
+function goBack() {
+    $('.movie_detail').hide()
+    $('.search').show()
+}
 
 
 doRequestMain(mainFunc)
-
-
-
-
-
-
-
-
-
-
-//  let totalPages = showData.total_pages
-//  let totalResults = showData.total_results
-//  let currentPage = showData.page
- 
-//  let pagination = "<button onclick='' disabled>&laquo;</button>";
-
- 
-//  pagination += "<button onclick='changePage("+currentPage+ "," + year + "," + genre + ")'>&raquo;</button>";
- 
- 
-//  $(".pagination_center").removeClass("display_none")
-//  document.getElementById("pagination").innerHTML = pagination;
-
-
-
-
-
-
-
-
-
-
-// function changePage(currentPage, year, genre) {
-//     console.log(year, genre, currentPage)
-//     let nextPage = currentPage + 1
-    
-    
-// changePageRequest.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=e5dce9ac19487be2b65ceb7be99e8ca7&with_genres=" + genreIds+"&primary_release_date.lte="+year)
-
-// changePageRequest.send();
-
-// changePageRequest.onreadystatechange= function (){
-//     if (this.readyState == 4 && this.status == 200){
-//         changePageInnerFunction(this.responseText)
-//     }
-// }
-
-
-// }
-         
-//  if (totalResults == 0) {
-//      pagination = ""
-//  }
- 
-//  if (totalPages == 1 ) {
-//      pagination += "<a href='#' class='active'>"+1+"</a>"
-//  }
- 
-//  if (totalPages == 2) {
-//      pagination += "<a href='#' class='active'>"+2+"</a>"
-//  }
- 
-
- 
-//  if (totalResults <=40 && totalResults ) {
-//      pagination += "<a href='#' class='active'>"+2+"</a>"
-//  }
-//  for (let i = 1; i <= totalPages; i++) {
-//     if (i < 10) {
-//         pagination += "<a href='#'>"+[i]+"</a>"
-//     }else {
-//         return
-//     }
-//  }
-        
-        
-        
-// function langSelect(year, genresIds) {
-    
-//     function langSelectInnerFunction(apiData) {
-        
-//         let langData = JSON.parse(apiData)
-//         document.getElementById('movie_count').innerHTML = langData.total_results
-//         let langCanvas = document.getElementById('lang_canvas')
-//         let langSelection ="<h3>Choose A Language</h3>"
-//          for (let i = 0; i <= langData.results.length -1; i++) {
-//              let langResult = langData.results[i].original_language
-//              console.log(langResult)
-//             langSelection += "<button class='btn btn-danger' onclick='showFilms(" + genreIds + "," + year + "," + langResult + ")'>" + langResult + "</button>"
-//          }
-         
-//          document.getElementById('lang_canvas').innerHTML = langSelection
-        
-//     }
-    
-    
-    
-//     langRequest.open("GET", "https://api.themoviedb.org/3/discover/movie?api_key=e5dce9ac19487be2b65ceb7be99e8ca7&with_genres=" + genreIds+"&primary_release_date.lte="+year)
-    
-//     langRequest.send();
-
-//     langRequest.onreadystatechange= function (){
-//         if (this.readyState == 4 && this.status == 200){
-//             langSelectInnerFunction(this.responseText)
-//         }
-//     }
-// }
-
